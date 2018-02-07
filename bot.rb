af7dfc7861ca98c54bffd1e929635fe6d39cafce
Chewsterchew <chew@chew.pw>
2018-02-06 18:03:30 -0600

    hold on while I overhaul the bot.rb

--- bot.rb
+++ bot.rb
@@ -1,45 +1,66 @@
-require 'cinch'
-require 'rest-client'
-require 'json'
-require 'YAML'
+# Require Gems needed to run programs and plugins
+require './requiregems.rb'
 
-CONFIG = YAML.load_file('config.yaml')
-
-class NickServ
-  include Cinch::Plugin
-
-  listen_to :connect, method: :identify
-
-  def identify(_m)
-    User('NickServ').send("identify #{CONFIG['nickserv']}")
-  end
+# Load config from file
+begin
+  CONFIG = YAML.load_file('config.yaml')
+rescue StandardError
+  puts 'Config file not found, this is fatal.'
+  exit
 end
 
-class Main
-  include Cinch::Plugin
+# Require each plugin
+Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each { |file| require file }
 
-  match /HowMuchLitecoinHasChewMined/, method: :HowMuchLitecoinHasChewMined
+# Pre-Config
+botnick = "HowMuchLitecoinHas#{CONFIG['miner']}Mined"
 
-  def HowMuchLitecoinHasChewMined(m)
-    api = JSON.parse(RestClient.get("https://www.litecoinpool.org/api?api_key=#{CONFIG['api']}"))
-    earned = api['user']['total_rewards']
-    hashes = api['user']['total_work']
-    price = api['market']['ltc_usd']
-    m.reply "Chew has mined $#{earned * price} worth of Litecoin. (LTC: $#{price}) (Mined: #{earned}) (Hashes Solved: #{hashes})"
-  end
-end
+botserver = if CONFIG['server'] == '' || CONFIG['server'].nil?
+              puts 'You did not configure a server for the bot to connect to. Please set one!'
+              exit
+            else
+              CONFIG['server'].to_s
+            end
+
+botport = if CONFIG['port'].nil?
+            '6667'
+          else
+            CONFIG['port']
+          end
+
+commits = `git rev-list master | wc -l`.to_i
+commit = if commits.zero?
+           ''
+         else
+           " | Version: #{commits}"
+         end
+
+botrealname = "HowMuchLitecoinHasChewMined Bot - https://github.com/Chewbotcca/HowMuchLitecoinHasChewMined#{commit}"
+
+botssl = if CONFIG['ssl'].nil? || CONFIG['ssl'] == '' || CONFIG['ssl'] == 'false' || CONFIG['ssl'] == false
+           nil
+         else
+           'true'
+         end
+
+botserverpass = if CONFIG['serverpass'].nil? || CONFIG['serverpass'] == ''
+                  nil
+                else
+                  CONFIG['serverpass']
+                end
 
 # Configure the Bot
 bot = Cinch::Bot.new do
   configure do |c|
-    c.nick = 'HowMuchLitecoinHasChewMined'
-    c.server = 'kitty.chew.chat'
-    c.channels = ['#lobby,#anarchy,#chewsmemechamber']
-    c.port = '6697'
-    c.user = 'Litecoins'
-    c.realname = 'oh snap'
+    # Bot Settings, Taken from pre-config
+    c.nick = botnick
+    c.server = botserver
+    c.channels = [CONFIG['channels']]
+    c.port = botport
+    c.realname = botrealname
     c.messages_per_second = 5
-    c.ssl.use = true
+    c.ssl.use = botssl
+    c.password = botserverpass
     c.plugins.prefix = //
 
     c.plugins.plugins = [Main, NickServ]
