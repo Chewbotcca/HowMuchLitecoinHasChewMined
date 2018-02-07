fa91d70f48cf3c1250b02f32b2d7efd0d268da8f
Chewsterchew <chew@chew.pw>
2018-02-06 18:03:48 -0600

    split main off into its own plugin and modify some things

---
+++ plugins/main.rb
@@ -0,0 +1,13 @@
+class Main
+  include Cinch::Plugin
+
+  match /HowMuchLitecoinHas#{CONFIG['miner']}Mined/, method: :checkstats
+
+  def checkstats(m)
+    api = JSON.parse(RestClient.get("https://www.litecoinpool.org/api?api_key=#{CONFIG['api']}"))
+    earned = api['user']['total_rewards']
+    hashes = api['user']['total_work']
+    price = api['market']['ltc_usd']
+    m.reply "#{CONFIG['miner']} has mined $#{earned * price} worth of Litecoin. (LTC: $#{price}) (Mined: #{earned}) (Hashes Solved: #{hashes})"
+  end
+end
